62ddd1a4c15cf3f3511e1cb2ae1b1ea29428e7e8
Mauricio Ferrari <m10ferrari1200@gmail.com>
2026-05-23 16:20:36 -0300

    change help

--- src/help.rs
+++ src/help.rs
@@ -3,40 +3,143 @@
 //! Provides structures to document commands and a formatter to print
 //! the generated help information to the console.
 
-pub struct ArgHelp<'a> {
-    pub short: Option<&'a str>,
-    pub long: &'a str,
-    pub description: &'a str,
-    pub is_subcommand: bool,
+use crate::messages::get_app_name;
+use crate::{Arg, NULL_PTR};
+
+///
+pub struct AppProperties<'a> {
+    ///
+    pub name: &'a str,
+    ///
+    pub desc: &'a str,
+    ///
+    pub version: &'a str,
 }
 
+/// todo completar descrição
+pub struct ArgHelp<'a> {
+    ///
+    pub short: Option<&'a str>,
+    ///
+    pub long: &'a str,
+    ///
+    pub desc: &'a str,
+    ///
+    pub is_subcommand: bool,
+    ///
+    pub properties: Option<AppProperties<'a>>,
+}
+
+/// todo descrição
 impl<'a> ArgHelp<'a> {
-    pub fn arg(short: Option<&'a str>, long: &'a str, desc: &'a str) -> Self {
-        Self { short, long, description: desc, is_subcommand: false }
+    /// todo descrição argumento
+    pub const fn properties(name: &'a str, desc: &'a str, ver: &'a str) -> Self {
+        Self {
+            short: None,
+            long: NULL_PTR,
+            desc: NULL_PTR,
+            is_subcommand: false,
+            properties: Some(AppProperties {
+                name,
+                desc,
+                version: ver,
+            }),
+        }
     }
 
-    pub fn subcommand(long: &'a str, desc: &'a str) -> Self {
-        Self { short: None, long, description: desc, is_subcommand: true }
+    /// todo descrição argumento
+    pub const fn arg(short: Option<&'a str>, long: &'a str, desc: &'a str) -> Self {
+        Self {
+            short,
+            long,
+            desc,
+            is_subcommand: false,
+            properties: None,
+        }
+    }
+
+    /// todo descrição argumento
+    pub const fn subcommand(short: Option<&'a str>, long: &'a str, desc: &'a str) -> Self {
+        Self {
+            short,
+            long,
+            desc,
+            is_subcommand: true,
+            properties: None,
+        }
     }
 }
 
 /// Prints the help information based on the provided rules.
-pub fn print_help(sub: &str, rules: &[ArgHelp]) {
-    let app_name = std::env::current_exe()
-        .unwrap_or_default()
-        .file_name()
-        .unwrap_or_default()
-        .to_string_lossy()
-        .to_string();
+/// todo completar argumento
+pub(crate) fn print_help(
+    sub: &str,
+    props: Option<&AppProperties>,
+    rules: &[Arg],
+    help_rules: &[ArgHelp],
+) {
+    let app_name = get_app_name();
 
-    println!("Usage: {} {} [OPTIONS]", app_name, sub);
-    println!("\nOptions:");
-
-    for rule in rules {
-        let flag = match rule.short {
-            Some(s) => format!("{}, {}", s, rule.long),
-            None => rule.long.to_string(),
-        };
-        println!("  {:<20} {}", flag, rule.description);
+    if let Some(p) = props {
+        println!("{} - {}\n\n{}\n", app_name, p.name, p.desc);
+    } else {
+        println!("{}\n", app_name);
     }
+
+    let active_help: Vec<&ArgHelp> = help_rules
+        .iter()
+        .filter(|h| {
+            rules.iter().any(|r| {
+                let long_match = r.long.split('|').any(|part| part == h.long);
+                let short_match = match (h.short, r.short) {
+                    (Some(h_short), Some(r_short)) => h_short == r_short,
+                    (None, _) => true,
+                    (Some(_), None) => false,
+                };
+                long_match && short_match
+            })
+        })
+        .collect();
+
+    let subcommands: Vec<&&ArgHelp> = active_help.iter().filter(|h| h.is_subcommand).collect();
+    let sub_placeholder = if !subcommands.is_empty() {
+        "[SUBCOMMAND] "
+    } else {
+        ""
+    };
+    let place = if sub.is_empty() {
+        sub_placeholder
+    } else {
+        &format!("{} ", sub)
+    };
+
+    println!("Usage: {} {}[OPTIONS]", app_name, place,);
+
+    if !subcommands.is_empty() {
+        println!("\nSubcommands:");
+        for help in subcommands {
+            let flag = match help.short {
+                Some(s) => format!("{:>2}, {}", s, help.long),
+                None => format!("{:>4}{}", NULL_PTR, help.long),
+            };
+            println!("  {:<30}{}", flag, help.desc);
+        }
+    }
+
+    let options: Vec<&&ArgHelp> = active_help.iter().filter(|h| !h.is_subcommand).collect();
+    if !options.is_empty() {
+        println!("\nOptions:");
+        for help in options {
+            let flag = match help.short {
+                Some(s) => format!("{:>2}, {}", s, help.long),
+                None => format!("{:>4}{}", NULL_PTR, help.long),
+            };
+            println!("  {:<30}{}", flag, help.desc);
+        }
+    }
+
+    println!("\nGeneral Options:");
+    println!("  {:<30}Show this help message", "-h, --help");
+    println!("  {:<30}Show this all help message", "    --help-all");
+    println!("  {:<30}Show version", "-V, --version");
 }
